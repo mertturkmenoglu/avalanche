ebb0aff93b8771516e2f42fc01fb3688e5d42481
Mert <mertturkmenoglu99@gmail.com>
2021-04-04 18:52:09 +0300

    Change login page app name text styling

--- components/LoginMotto.tsx
+++ components/LoginMotto.tsx
@@ -2,7 +2,7 @@ const LoginMotto = (): JSX.Element => {
   return (
     <div className="mr-auto my-auto text-center">
       <img src="/mountains.svg" alt="Logo" className="h-48 mx-auto" />
-      <h1 className="text-5xl font-light text-center text-gray-200">
+      <h1 className="text-5xl font-light text-center text-gray-200 tracking-widest">
         Avalanche
       </h1>
       <h1 className="font-thin text-2xl mt-4 text-gray-200">
