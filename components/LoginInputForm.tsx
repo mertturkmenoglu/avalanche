6abd61af74b729404deacd1d9209862a7f0fa96c
Mert <mertturkmenoglu99@gmail.com>
2021-04-04 18:45:14 +0300

    Update LoginPage styling and components

---
+++ components/LoginInputForm.tsx
@@ -0,0 +1,63 @@
+import React, { useState, FormEvent } from 'react'
+import Link from 'next/link'
+
+interface Props {
+  onFormSubmit: (e: FormEvent<HTMLFormElement>) => void
+}
+
+const LoginInputForm = ({ onFormSubmit }: Props): JSX.Element => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  return (
+    <form
+      onSubmit={onFormSubmit}
+      className="w-full max-w-md ml-36 mr-auto my-auto bg-gray-50 rounded-lg shadow-lg py-10 px-12"
+    >
+      <h2 className="text-2xl font-extralight mb-8 text-center text-indigo-900">
+        Login
+      </h2>
+      <div>
+        <label htmlFor="email" className="font-thin">
+          Email
+        </label>
+        <input
+          type="email"
+          className="w-full p-2 text-sm border rounded-md outline-none transition duration-150 ease-in-out mb-4 focus:outline-none focus:ring-1 focus:ring-indigo-900"
+          id="email"
+          placeholder="Your email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="password" className="font-thin">
+          Password
+        </label>
+        <input
+          type="password"
+          className="w-full p-2 text-sm border rounded-md outline-none transition duration-150 ease-in-out mb-4 focus:outline-none focus:ring-1 focus:ring-indigo-900"
+          id="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+
+      <div className="flex justify-center items-center mt-6">
+        <button className="bg-indigo-700 py-2 px-4 text-sm text-white rounded focus:outline-none hover:bg-indigo-900">
+          Login
+        </button>
+      </div>
+
+      <div className="font-thin text-gray-500 mt-4 text-center">
+        <Link href="/forget-password">Forgot password?</Link>
+      </div>
+      <div className="font-thin text-gray-500 mt-4 text-center">
+        <Link href="/register">Need an account?</Link>
+      </div>
+    </form>
+  )
+}
+
+export default LoginInputForm
---
+++ components/LoginMotto.tsx
@@ -0,0 +1,15 @@
+const LoginMotto = (): JSX.Element => {
+  return (
+    <div className="mr-auto my-auto text-center">
+      <img src="/mountains.svg" alt="Logo" className="h-48 mx-auto" />
+      <h1 className="text-5xl font-light text-center text-gray-200">
+        Avalanche
+      </h1>
+      <h1 className="font-thin text-2xl mt-4 text-gray-200">
+        Change your world
+      </h1>
+    </div>
+  )
+}
+
+export default LoginMotto
