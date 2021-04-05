d1f6ffdb3536756c32885fe9b11cbe1bbb4f0bb8
Mert <mertturkmenoglu99@gmail.com>
2021-04-05 15:52:36 +0300

    Add meta tags for SEO

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -16,6 +16,16 @@ const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
     <AppContext.Provider value={values}>
       <Head>
         <title>Avalanche</title>
+        <meta name="description" content="Avalanche Project Management" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="copyright" content="Mert Turkmenoglu" />
+        <meta property="og:image" content="/mountainsIndigo.svg" />
+        <meta property="og:locale" content="en_GB" />
+        <meta
+          name="keywords"
+          content="avalanche, project, management, project management, agile, teams"
+        />
+        <link rel="icon" type="image/svg" href="/mountainsIndigo.svg" />
       </Head>
       <Component {...pageProps} />
     </AppContext.Provider>
