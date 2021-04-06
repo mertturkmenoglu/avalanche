158e6804b839d381ee2074bd97aec1e0f1dd4149
Mert <mertturkmenoglu99@gmail.com>
2021-04-06 12:58:10 +0300

    Add landing page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -19,13 +19,13 @@ const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
         <meta name="description" content="Avalanche Project Management" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="copyright" content="Mert Turkmenoglu" />
-        <meta property="og:image" content="/mountainsIndigo.svg" />
+        <meta property="og:image" content="/mountains.svg" />
         <meta property="og:locale" content="en_GB" />
         <meta
           name="keywords"
           content="avalanche, project, management, project management, agile, teams"
         />
-        <link rel="icon" type="image/svg" href="/mountainsIndigo.svg" />
+        <link rel="icon" type="image/svg" href="/mountains.svg" />
       </Head>
       <Component {...pageProps} />
     </AppContext.Provider>
