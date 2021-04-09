191c6f6d548839d967a3ad2bc5a6e1a038e9457b
Mert <mertturkmenoglu99@gmail.com>
2021-04-09 19:58:39 +0300

    Add CTA to landing page. Closes #3.

--- pages/landing.tsx
+++ pages/landing.tsx
@@ -37,7 +37,45 @@ const LandingPage = (): JSX.Element => {
           </Link>
         </div>
       </div>
-      <div id="hero" className="mt-24 grid grid-cols-10 gap-20 bg-indigo-50">
+
+      <div className="block md:flex md:items-center mt-8 md:mt-24 md:w-full">
+        <div className="w-11/12 sm:w-64 h-64 lg:w-96 bg-blue-700 flex ml-auto mr-auto md:ml-12 md:mr-2 rounded-lg ">
+          {/* Photo by ian dooley on Unsplash */}
+          <Image
+            className="object-cover rounded-lg opacity-50 bg-blue-500"
+            src="/hero.jpg"
+            width={640}
+            height={256}
+          />
+        </div>
+
+        <div className="w-11/12 sm:w-64 md:w-64 bg-gradient-to-tr from-blue-500 to-blue-900 h-64 rounded-lg flex flex-1 mt-4 md:mt-0 mx-auto md:mx-0 md:mr-12">
+          <div className="block md:flex md:justify-between pt-16 md:pt-0 mx-auto md:mx-0 items-center md:ml-8 w-full">
+            <div className="text-center md:text-left">
+              <div className="text-3xl text-white font-semibold uppercase tracking-wide leading-8  hover:bg-yellow-300 hover:text-white ease-in-out delay-200 transform transition duration-1000 hover:scale-105 hover:-skew-x-12">
+                Avalanche
+              </div>
+              <div className="text-base text-white">
+                Productivity, <span className="underline">re-designed</span>
+              </div>
+            </div>
+            <div className="text-center mt-6 md:mt-0 md:ml-8 md:mr-8 lg:ml-auto lg:mr-24">
+              <button className="bg-yellow-300 px-6 py-2 rounded-full text-gray-700 focus:outline-none focus:ring-1 focus:ring-gray-300">
+                <Link href="/register">Get Started</Link>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="w-full">
+        <ChevronDoubleDownIcon className="h-8 w-8 mx-auto mt-24 mb-12 text-indigo-900 animate-bounce" />
+      </div>
+
+      <div
+        id="hero"
+        className="mx-24 mt-24 grid grid-cols-10 gap-20 bg-indigo-50"
+      >
         <div className="space-y-4 col-span-3 col-start-3 mt-36">
           <div className="text-gray-600 text-7xl font-semibold tracking-wide hover:bg-yellow-300 hover:text-white ease-in-out delay-200 transform transition duration-1000 hover:scale-105 hover:-skew-x-12">
             Avalanche
@@ -58,9 +96,7 @@ const LandingPage = (): JSX.Element => {
           </div>
         </div>
       </div>
-      <div className="w-full">
-        <ChevronDoubleDownIcon className="h-8 w-8 mx-auto mt-24 mb-12 text-indigo-900 animate-bounce" />
-      </div>
+
       {/* <hr className="mx-32 border border-indigo-200" /> */}
     </div>
   )
