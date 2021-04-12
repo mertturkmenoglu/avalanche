e2a15b2705df0e808030426b5ab367250ffc5814
Mert <mertturkmenoglu99@gmail.com>
2021-04-12 18:48:37 +0300

    Add Express session middleware. Closes #11.

---
+++ server/src/constants.ts
@@ -0,0 +1,6 @@
+/* eslint-disable import/prefer-default-export */
+export const COOKIE_NAME = 'aid';
+
+export const isProd = process.env.NODE_ENV === 'production';
+
+export const FRONTEND_DOMAIN = 'projectavalanche.vercel.app';
--- server/src/index.ts
+++ server/src/index.ts
@@ -4,11 +4,23 @@ import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
 import { buildSchema } from 'type-graphql';
 import cors from 'cors';
+import session from 'express-session';
+import Redis from 'ioredis';
+import connectRedis from 'connect-redis';
+import dotenv from 'dotenv-safe';
 
 import HelloResolver from './resolvers/HelloResolver';
+import { COOKIE_NAME, FRONTEND_DOMAIN, isProd } from './constants';
+
+dotenv.config();
 
 const main = async () => {
   const app = express();
+  const RedisStore = connectRedis(session);
+  const redis = new Redis();
+
+  app.set('trust proxy', 1);
+
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
       resolvers: [HelloResolver],
@@ -17,12 +29,31 @@ const main = async () => {
     context: ({ req, res }) => ({
       req,
       res,
+      redis,
     }),
   });
 
   app.use(cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true,
+  }));
+
+  app.use(session({
+    name: COOKIE_NAME,
+    secret: process.env.SESSION_SECRET as string,
+    store: new RedisStore({
+      client: redis,
+      disableTouch: true,
+    }),
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24 * 90, // 90 days
+      httpOnly: true,
+      sameSite: 'lax',
+      secure: isProd,
+      domain: isProd ? FRONTEND_DOMAIN : undefined,
+    },
+    saveUninitialized: false,
+    resave: false,
   }));
 
   apolloServer.applyMiddleware({
