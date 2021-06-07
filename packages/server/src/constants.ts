adc302a67588d5dd63be42f3d65cb4253f1406ce
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2021-06-07 18:57:21 +0300

    Remove old server codes

--- packages/server/src/constants.ts
+++
@@ -1,6 +0,0 @@
-/* eslint-disable import/prefer-default-export */
-export const COOKIE_NAME = 'aid';
-
-export const isProd = process.env.NODE_ENV === 'production';
-
-export const FRONTEND_DOMAIN = 'projectavalanche.vercel.app';
