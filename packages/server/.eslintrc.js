fc888d9dfbbc0a6ae2c5b3dcef1c57a7cbb45c87
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2021-06-07 19:07:39 +0300

    Add Nest.js to project

---
+++ packages/server/.eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  ignorePatterns: ['.eslintrc.js'],
+  rules: {
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    "no-console": "warn",
+    "indent": ["error", 2],
+    "quotes": ["error", "single"],
+    "linebreak-style": "off",
+    "import/extensions": "off",
+    "workspaces/no-absolute-imports": "off"
+  },
+};
