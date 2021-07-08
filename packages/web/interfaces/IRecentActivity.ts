18d660969d99406016f2ab1d1ef2f524bbff7769
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2021-07-08 13:56:02 +0300

    Add home page model interfaces

---
+++ packages/web/interfaces/IRecentActivity.ts
@@ -0,0 +1,10 @@
+interface IRecentActivity {
+  user: {
+    name: string;
+    image: string;
+  }
+  description: string;
+  date: string;
+}
+
+export default IRecentActivity;
---
+++ packages/web/interfaces/ITodayEvent.ts
@@ -0,0 +1,7 @@
+interface ITodayEvent {
+  description: string;
+  date: string;
+  color: string;
+}
+
+export default ITodayEvent;
