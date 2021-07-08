53f2b96e743c266b42ab6b2d16ff1dcde0037f85
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2021-07-08 13:56:51 +0300

    Add pages

---
+++ packages/web/pages/about.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const AboutPage = (): JSX.Element => (
+  <div>AboutPage</div>
+);
+
+export default AboutPage;
---
+++ packages/web/pages/api.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ApiPage = (): JSX.Element => (
+  <div>ApiPage</div>
+);
+
+export default ApiPage;
