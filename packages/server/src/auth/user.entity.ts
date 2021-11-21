0a529292f12512862280c4589d89b95c7b9aabc1
Mert Türkmenoğlu <mertturkmenoglu99@gmail.com>
2021-11-21 22:54:06 +0300

    Add Swagger to project

--- packages/server/src/auth/user.entity.ts
+++ packages/server/src/auth/user.entity.ts
@@ -1,19 +1,25 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class User {
+  @ApiProperty()
   @PrimaryGeneratedColumn()
   id!: number;
 
+  @ApiProperty()
   @Column({ nullable: false })
   firstName!: string;
 
+  @ApiProperty()
   @Column({ nullable: false })
   lastName!: string;
 
+  @ApiProperty()
   @Column({ unique: true, nullable: false, length: '255' })
   email!: string;
 
+  @ApiProperty()
   @Column({ unique: true, nullable: false, length: '32' })
   username!: string;
 }
